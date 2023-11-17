ff43c72b9b207360cfb6a745546c808fd60555bc
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-17 22:13:51 +0200

    Added urls and views

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = [
-    # path('', views.add_photo, name="photo-add"),
-    # path('profile/<int:pk>/', include([
-    #     path('', views.show_photo_details, name='photo-details'),
-    #     path('edit/', views.edit_photo, name='photo-edit'),
-    #
-    # ])),
+    path('', views.add_photo, name="photo-add"),
+    path('add/', views.add_photo, name='photo-add'),
+    path('<int:pk>/', include([
+        path('', views.show_photo_details, name='photo-details'),
+        path('edit/', views.edit_photo, name='photo-edit'),
+
+
+     ])),
 ]
