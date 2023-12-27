b7ffb1c00d0f31ab985a163976df40837fcec5c8
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-27 19:55:06 +0200

    Added base html and template inheritance

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = [
-    path('', views.add_photo, name="photo-add"),
     path('add/', views.add_photo, name='photo-add'),
     path('<int:pk>/', include([
         path('', views.show_photo_details, name='photo-details'),
