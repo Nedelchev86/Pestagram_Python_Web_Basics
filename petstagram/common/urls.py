6034e695448664e2359d2fae19bc7a1f4ecb2fc9
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-27 21:11:14 +0200

    Added base html and template inheritance

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from petstagram.common import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('like/<int:photo_id>', views.like_functionality, name="like"),
 ]
